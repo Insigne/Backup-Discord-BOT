6a5111cc6ec6d24cdde523dbbf3dc46bf8f2bff9
Insigne <insigneeee@gmail.com>
2020-12-20 02:28:50 +0100

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,8 +1,3 @@
 module.exports = async(client) => {
-    
-    let activities = [ `${client.guilds.cache.size} servers | m!help` ], i = 0;
-    setInterval(() => client.user.setActivity(`${activities[i++ % activities.length]}`, { type: "WATCHING" }), 15000);
-
     console.log(`${client.user.username} online!`);
-    setInterval(() => console.log(`${client.user.username} currently online on ${client.guilds.cache.size} servers with ${client.users.cache.size} user.`), 15000);
 }
